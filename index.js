46166f1e425fff02b2bc9c2cfa5f7163917fcb70
hmtb <martin.hering.it@gmail.com>
2022-10-03 11:12:17 +0200

    switch only once per 5 secondes

--- index.js
+++ index.js
@@ -8,22 +8,27 @@ prompt.start();
 
 // add nfc tags
 const config = require('./config.json')
-
+let lastCommand = Date.now()
 function waitForInput() {
   prompt.get(['key'], async (err, result) => {
     if (err) {
       throw err;
     }
-    if (has(config.tags, result.key)) {
+    if (Date.now() < lastCommand + 5000) {
+      console.log('Only once Paula');
+    }
+    else if (has(config.tags, result.key)) {
       console.log('play', result.key, config.tags[result.key]);
 
 
       try {
         const clearResponse = await axios.get(`http://localhost:5005/${config.speaker}/clearqueue`);
         console.log("clear queue", clearResponse.data.status);
-        
+
         const response = await axios.get(`http://localhost:5005/${config.speaker}/spotify/now/` + config.tags[result.key].uri)
-        console.log(`play ${config.tags[result.key].name}`,response.data.status);
+        console.log(`play ${config.tags[result.key].name}`, response.data.status);
+
+        lastCommand = Date.now()
       } catch (error) {
         // handle error
         console.log(error);
