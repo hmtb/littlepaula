7ffb4e08aba6f4e61143b29514544274b7f27c24
hmtb <martin.hering.it@gmail.com>
2022-10-03 10:57:42 +0200

    do not stack songs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const prompt = require('prompt');
 const axios = require('axios');
-const { has} = require('lodash')
+const { has } = require('lodash')
 
 // start the prompt
 prompt.start();
@@ -10,25 +10,30 @@ prompt.start();
 const config = require('./config.json')
 
 function waitForInput() {
-  prompt.get(['key'], (err, result) => {
+  prompt.get(['key'], async (err, result) => {
     if (err) {
-        throw err;
+      throw err;
     }
-    if(has(config.tags,result.key)) {
-        console.log('send', result.key, config.tags[result.key]);
-        axios.get(`http://localhost:5005/${config.speaker}/spotify/now/` + config.tags[result.key].uri).then(function (response) {
-        // handle success
-        console.log(response.data.status);
-      }).catch(function (error) {
+    if (has(config.tags, result.key)) {
+      console.log('play', result.key, config.tags[result.key]);
+
+
+      try {
+        const clearResponse = await axios.get(`http://localhost:5005/${config.speaker}/clearqueue`);
+        console.log("clear queue", clearResponse.data.status);
+        
+        const response = await axios.get(`http://localhost:5005/${config.speaker}/spotify/now/` + config.tags[result.key].uri)
+        console.log(`play ${config.tags[result.key].name}`,response.data.status);
+      } catch (error) {
         // handle error
         console.log(error);
-      })
+      }
     } else {
       console.log("tag not found");
     }
     // wait for next input
     waitForInput()
-});
+  });
 }
 // ask user for the input
 waitForInput()
